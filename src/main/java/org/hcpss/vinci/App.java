1a74db8419dd7c6a91f225a3cb4897b684b5d029
Emilyn Heikell <citrusemmytick@gmail.com>
2022-05-11 07:26:07 -0400

    Init

---
+++ src/main/java/org/hcpss/vinci/App.java
@@ -0,0 +1,5 @@
+package org.hcpss.vinci;
+
+public final class App {
+    
+}
---
+++ src/main/java/org/hcpss/vinci/Window.java
@@ -0,0 +1,7 @@
+package org.hcpss.vinci;
+
+import javax.swing.JFrame;
+
+public class Window extends JFrame {
+    
+}
